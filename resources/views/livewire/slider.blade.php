91c5784c094456cc8b3e1149ccd5d66450124d5a
Jose <jmgarcia81@uoc.edu>
2021-02-24 17:01:16 +0100

    Slider + Cards (no responsive)

--- resources/views/livewire/slider.blade.php
+++ resources/views/livewire/slider.blade.php
@@ -1,37 +1,36 @@
+<section class="container">
 
-<div id="carrusel">
-    <a href="#" class="left-arrow"><img src=".public/images/Left"></a>
-    <a href="#" class="right-arrow"><img src=".public/images/Right"></a>
-    <div class="carrusel">
-        <div class="product" id="product_0">
-            <img src="https://storage.googleapis.com/webdesignledger.pub.network/WDL/work-better-with-coders-1.jpg" width="195" height="100" />
-            <h5>Lorem ipsum 1</h5>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta amet iusto saepe dolorem assumenda. Autem adipisci quia animi facilis corporis qui cum aperiam mollitia? Repellendus non quam voluptate libero delectus?</p>
-        </div>
-        <div class="product" id="product_1">
-            <img src="https://storage.googleapis.com/webdesignledger.pub.network/WDL/work-better-with-coders-1.jpg" width="195" height="100" />
-            <h5>Lorem ipsum 2</h5>
-            <p>100.00 €</p>
-        </div>
-        <div class="product" id="product_2">
-            <img src="https://storage.googleapis.com/webdesignledger.pub.network/WDL/work-better-with-coders-1.jpg" width="195" height="100" />
-            <h5>Lorem ipsum 3</h5>
-            <p>250.00 €</p>
-        </div>
-        <div class="product" id="product_3">
-            <img src="https://storage.googleapis.com/webdesignledger.pub.network/WDL/work-better-with-coders-1.jpg" width="195" height="100" />
-            <h5>Lorem ipsum 4</h5>
-            <p>75.00 €</p>
-        </div>
-        <div class="product" id="product_4">
-            <img src="https://storage.googleapis.com/webdesignledger.pub.network/WDL/work-better-with-coders-1.jpg" width="195" height="100" />
-            <h5>Lorem ipsum 5</h5>
-            <p>65.00 €</p>
-        </div>
-        <div class="product" id="product_5">
-            <img src="https://storage.googleapis.com/webdesignledger.pub.network/WDL/work-better-with-coders-1.jpg" width="195" height="100" />
-            <h5>Lorem ipsum 6</h5>
-            <p>40.00 €</p>
-        </div>
-    </div>
-</div>
+    <a href="#" id="left-arrow"><img src="images/Left.png" /></a>
+    <a href="#" id="right-arrow"><img src="images/Right.png" /></a>
+
+    <div class="card">
+        <img class= "picture" src="https://storage.googleapis.com/webdesignledger.pub.network/WDL/work-better-with-coders-1.jpg" alt="Denim Jeans" style="width:100%">
+        <h1>TALLER 1</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi dignissimos iste ex quasi quisquam et non porro hic ipsam tempora? Maiores vel impedit sint eum illo adipisci quod quas? Sequi?</p>
+        <button>INSCRÍBETE</button>
+      </div>
+
+      <div class="card">
+        <img class= "picture" src="https://storage.googleapis.com/webdesignledger.pub.network/WDL/work-better-with-coders-1.jpg" alt="Denim Jeans" style="width:100%">
+        <h1>Taller 2</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi dignissimos iste ex quasi quisquam et non porro hic ipsam tempora? Maiores vel impedit sint eum illo adipisci quod quas? Sequi?</p>
+        <button>INSCRÍBETE</button>
+      </div>
+
+      <div class="card">
+        <img class= "picture" src="https://storage.googleapis.com/webdesignledger.pub.network/WDL/work-better-with-coders-1.jpg" alt="Denim Jeans" style="width:100%">
+        <h1>Taller 3</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi dignissimos iste ex quasi quisquam et non porro hic ipsam tempora? Maiores vel impedit sint eum illo adipisci quod quas? Sequi?</p>
+        <button>INSCRÍBETE</button>
+      </div>
+
+      <div class="card">
+        <img class= "picture" src="https://storage.googleapis.com/webdesignledger.pub.network/WDL/work-better-with-coders-1.jpg" alt="Denim Jeans" style="width:100%">
+        <h1>Taller 4</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi dignissimos iste ex quasi quisquam et non porro hic ipsam tempora? Maiores vel impedit sint eum illo adipisci quod quas? Sequi?</p>
+        <button>INSCRÍBETE</button>
+      </div>
+
+</section>
+
+

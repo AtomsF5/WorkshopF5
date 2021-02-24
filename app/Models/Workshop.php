f89101ca3b6f95b48dd193b8c8d786df7b439b2d
Mercedes <mvazquezsan@gmail.com>
2021-02-24 14:56:41 +0100

    db fields included in  Workshop Model

--- app/Models/Workshop.php
+++ app/Models/Workshop.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Workshop extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'hour',   
+        'date',            
+        'technical_requirement',
+        'image',
+        'platform_web'
+    ];
+
 }

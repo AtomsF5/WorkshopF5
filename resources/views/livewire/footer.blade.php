c14f0b8ca155db94baf831bb7062c8f6a49b5aaf
Olga <o.jpadilla73@gmail.com>
2021-02-24 21:01:46 +0100

    Estructura Footer

--- resources/views/livewire/footer.blade.php
+++ resources/views/livewire/footer.blade.php
@@ -1,25 +1,28 @@
-<footer id="footer" class="bg-orangeF5">
+<footer id="footer" class="flex bg-orangeF5 h-25 absolute bottom-0 w-full">
 
     <!-- start container -->
-    <div class="container mx-auto pt-8 pb-4">
+    <div class="pt-8 pb-4">
 
-        <div class="flex flex-wrap overflow-hidden sm:-mx-1 md:-mx-px lg:-mx-2 xl:-mx-2">
+        <div class="overflow-hidden sm:-mx-1 md:-mx-px lg:-mx-2 xl:-mx-2">
 
-            <div class="pt-4 md:flex md:items-center md:justify-center " style="border-top:1px solid white">
-
-            <img style="max-width: 70%;height:auto;" class="" src="./images/logo-b.png" alt="Logo">
-
+            <div class="redes flex">
+                <img class="h-6 mx-2" src="./images/Face-b.png">
+                <img class="h-6 mx-2" src="./images/Insta-b.png">
+                <img class="h-6 mx-2" src="./images/Twitter-b.png">
+                <img class="h-6 mx-2" src="./images/Linkedin-b.png">
+                <img class="h-6 mx-2" src="./images/Youtube-b.png">
             </div>
 
-            <div class="w-full  sm:my-1 sm:px-1 sm:w-1/2 md:my-px md:px-px md:w-1/2 lg:my-2 lg:px-2 lg:w-1/4 xl:my-2 xl:px-2 xl:w-1/4 pb-6">
+            <div class="w-full  ">
 
                 <ul class="">
                     <li class="md:mx-2 md:inline leading-7 text-sm" id="footer-navi-2"><a class="text-white underline text-small" href="/cookie">Cookie policy</a></li>
-                    <li class="md:mx-2 md:inline leading-7 text-sm" id="footer-navi-2"><a class="text-white underline text-small" href="/privacy">Privacy</a></li>
+                    <li class="md:mx-2 md:inline leading-7 text-sm" id="footer-navi-2"><a class="text-white underline text-small" href="/privacy">Privacy policy</a></li>
                 </ul>
             </div>
 
         </div>
+
 
     </div>
 
--- resources/views/livewire/header.blade.php
+++ resources/views/livewire/header.blade.php
@@ -1,31 +1,24 @@
-<div>
-    <header id="header" class="bg-white">
+<header class="bg-white">
 
-        <!-- start container -->
-        <div class="container mx-auto pt-8 pb-4">
+    <div class="container mx-auto pt-8 pb-4">
 
-            <div class="flex flex-wrap overflow-hidden sm:-mx-1 md:-mx-px lg:-mx-2 xl:-mx-2">
+        <div class="flex flex-wrap justify-between overflow-hidden sm:-mx-1 md:-mx-px lg:-mx-2 xl:-mx-2">
 
-                <div class="pt-4 md:flex md:items-center md:justify-center " style="">
-
+            <div class="md:flex md:items-center md:justify-center ">
                 <img class="max-width: 50%;height:auto;" src="./images/logo-F5.png">
-
-                </div>
-
-                <div class="absolute text-orangeF5 z-10 top-7 left-10 w-1/2 lg:left-32 xl:top-40 xl:left-52">
-
-                    <p>#ROMPIENDOLOSCÓDIGOS</p>
-
-                </div>
-
-                <div class="redes">
-                    <img class="" src="./images/Facebook.png">
-                    <img class="" src="./images/Instagram.png">
-                    <img class="" src="./images/Twitter.png">
-                    <img class="" src="./images/Linkedin.png">
-                    <img class="" src="./images/Youtube.png">
-                </div>
             </div>
 
-    </header>
-</div>
+            <div class="redes flex">
+                <img class="h-6 mx-2" src="./images/Facebook.png">
+                <img class="h-6 mx-2" src="./images/Instagram.png">
+                <img class="h-6 mx-2" src="./images/Twitter.png">
+                <img class="h-6 mx-2" src="./images/Linkedin.png">
+                <img class="h-6 mx-2" src="./images/Youtube.png">
+            </div>
+        </div>
+        <div class="text-center text-orangeF5">
+
+            <p>#ROMPIENDOLOSCÓDIGOS</p>
+
+        </div>
+</header>

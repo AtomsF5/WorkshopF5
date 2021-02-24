9a24f2da2dce3983836488ba6f1e4b588e73aace
Jose <jmgarcia81@uoc.edu>
2021-02-24 09:26:55 +0100

    Initial commit

---
+++ resources/views/livewire/slider.blade.php
@@ -0,0 +1,37 @@
+
+<div id="carrusel">
+    <a href="#" class="left-arrow"><img src=".public/images/Left"></a>
+    <a href="#" class="right-arrow"><img src=".public/images/Right"></a>
+    <div class="carrusel">
+        <div class="product" id="product_0">
+            <img src="https://storage.googleapis.com/webdesignledger.pub.network/WDL/work-better-with-coders-1.jpg" width="195" height="100" />
+            <h5>Lorem ipsum 1</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta amet iusto saepe dolorem assumenda. Autem adipisci quia animi facilis corporis qui cum aperiam mollitia? Repellendus non quam voluptate libero delectus?</p>
+        </div>
+        <div class="product" id="product_1">
+            <img src="https://storage.googleapis.com/webdesignledger.pub.network/WDL/work-better-with-coders-1.jpg" width="195" height="100" />
+            <h5>Lorem ipsum 2</h5>
+            <p>100.00 €</p>
+        </div>
+        <div class="product" id="product_2">
+            <img src="https://storage.googleapis.com/webdesignledger.pub.network/WDL/work-better-with-coders-1.jpg" width="195" height="100" />
+            <h5>Lorem ipsum 3</h5>
+            <p>250.00 €</p>
+        </div>
+        <div class="product" id="product_3">
+            <img src="https://storage.googleapis.com/webdesignledger.pub.network/WDL/work-better-with-coders-1.jpg" width="195" height="100" />
+            <h5>Lorem ipsum 4</h5>
+            <p>75.00 €</p>
+        </div>
+        <div class="product" id="product_4">
+            <img src="https://storage.googleapis.com/webdesignledger.pub.network/WDL/work-better-with-coders-1.jpg" width="195" height="100" />
+            <h5>Lorem ipsum 5</h5>
+            <p>65.00 €</p>
+        </div>
+        <div class="product" id="product_5">
+            <img src="https://storage.googleapis.com/webdesignledger.pub.network/WDL/work-better-with-coders-1.jpg" width="195" height="100" />
+            <h5>Lorem ipsum 6</h5>
+            <p>40.00 €</p>
+        </div>
+    </div>
+</div>

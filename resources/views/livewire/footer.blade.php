f0f7f9d1d27f39d0c0b47ccc0cc6a28d360c7784
Olga <o.jpadilla73@gmail.com>
2021-02-25 16:06:56 +0100

    Arreglar footer

--- resources/views/livewire/footer.blade.php
+++ resources/views/livewire/footer.blade.php
@@ -1,6 +1,6 @@
 <footer id="footer" class="sm:column flex bg-orangeF5 h-30 absolute bottom-0 w-full">
 
-    <div class=" flex container mx-auto items-center h-full">
+    <div class=" flex justify-between container mx-auto items-center h-full">
 
         <div class="flex flex-col items-start">
 
@@ -12,20 +12,19 @@
                 <img class="h-6 mx-2" src="./images/Youtube-b.png">
             </div>
 
-            <div class="w-full flex leading-tight ml-2 mb-4 ">
-
-                <div class="font-bold" >
+            <div class="w-full flex flex-col leading-tight ml-2 mb-4 font-bold">
                     <a class="block text-white  text-small" href="/privacy">Privacy policy</a>
                     <a class="block text-white  text-small" href="/cookie">Cookie policy</a>
-                </div>
             </div>
         </div>
 
-        <div class="mx-1 flex flex-wrap overflow-hidden">
+        <div class="mx-1 flex flex-wrap flex-col text-right overflow-hidden">
 
-            <h3 class="text-white text-xl w-full overflow-hidden">CONTACTO</h3>
+            <h3 class="text-white text-xl overflow-hidden">CONTACTO</h3>
 
-            <h3 class="text-white  text-small w-full overflow-hidden">Depot Lab Coworking<br> (Carrer del Bruc, 149, 08037 Barcelona)<br>+34 658 56 30 97</h3>
+            <h3 class="text-white  text-small overflow-hidden">Depot Lab Coworking</h3>
+            <h3 class="text-white  text-small overflow-hidden">(Carrer del Bruc, 149, 08037 Barcelona)</h3>
+            <h3 class="text-white  text-small overflow-hidden">+34 658 56 30 97</h3>
 
         </div>
     </div>

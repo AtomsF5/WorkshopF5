e8888ffeb88d0727525142f8dc51f554ec620ee9
Olga <o.jpadilla73@gmail.com>
2021-02-25 15:27:53 +0100

    Merge

--- resources/views/livewire/footer.blade.php
+++ resources/views/livewire/footer.blade.php
@@ -1,8 +1,8 @@
-<footer id="footer" class="sm:column flex bg-orangeF5 h-25 absolute bottom-0 w-full">
+<footer id="footer" class="sm:column flex bg-orangeF5 h-30 absolute bottom-0 w-full">
 
     <div class=" flex container mx-auto items-center h-full">
 
-        <div class="flex w-1/3 flex-col items-start">
+        <div class="flex flex-col items-start">
 
             <div class="redes flex leading-tight mb-4">
                 <img class="h-6 mx-2" src="./images/Face-b.png">
@@ -21,11 +21,11 @@
             </div>
         </div>
 
-        <div class="mx-1 flex flex-wrap overflow-hidden ">
+        <div class="mx-1 flex flex-wrap overflow-hidden">
 
-            <h3 class="text-white text-xl w-full overflow-hidden" href="/contacto">CONTACTO</h3>
+            <h3 class="text-white text-xl w-full overflow-hidden">CONTACTO</h3>
 
-            <h3 class="text-white  text-small w-full overflow-hidden">Depot Lab Coworking<br> (Carrer del Bruc, 149, 08037 Barcelona)<br> +34 658 56 30 97</h3>
+            <h3 class="text-white  text-small w-full overflow-hidden">Depot Lab Coworking<br> (Carrer del Bruc, 149, 08037 Barcelona)<br>+34 658 56 30 97</h3>
 
         </div>
     </div>

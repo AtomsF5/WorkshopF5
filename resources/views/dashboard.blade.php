2e977e7411df215777c0c99a6576ddb88469b7d2
Mercedes <mvazquezsan@gmail.com>
2021-02-26 12:39:48 +0100

    data adapted to table to show in dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -17,14 +17,18 @@
 
             <table id="t01">
                 <tr>
-                  <th>Firstname</th>
-                  <th>Lastname</th>
-                  <th>Age</th>
+                  <th>Título</th>
+                  <th>Descripción</th>
+                  <th>Fecha</th>
+                  <th>Hora</th>
+                  <th>Requisitos Técnicos</th>
                 </tr>
                 <tr>
-                  <td>Eve</td>
-                  <td>Jackson</td>
-                  <td>94</td>
+                  <td>""</td>
+                  <td>""</td>
+                  <td>""</td>
+                  <td>""</td>
+                  <td>""</td>
                 </tr>
               </table>
 

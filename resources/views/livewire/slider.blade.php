6be0c0b8a62dcb0d5ff22c0d29978bd8623de9f3
Jose <jmgarcia81@uoc.edu>
2021-03-01 15:23:51 +0100

    Cards & Slider responsive ok

--- resources/views/livewire/slider.blade.php
+++ resources/views/livewire/slider.blade.php
@@ -1,7 +1,7 @@
-<div class="container">
+<div class="containerSlider">
 
-    <a href="#" id="left-arrow"><img src="images/Left.png" /></a>
-    <a href="#" id="right-arrow"><img src="images/Right.png" /></a>
+    {{-- <a href="#" id="left-arrow"><img src="images/Left.png" /></a>
+    <a href="#" id="right-arrow"><img src="images/Right.png" /></a> --}}
 
     
     <div class="card">
@@ -13,21 +13,21 @@
 
       <div class="card">
         <img class= "picture" src="https://storage.googleapis.com/webdesignledger.pub.network/WDL/work-better-with-coders-1.jpg" alt="Denim Jeans" style="width:100%">
-        <h1>Taller 2</h1>
+        <h1>TALLER 2</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi dignissimos iste ex quasi quisquam et non porro hic ipsam tempora? Maiores vel impedit sint eum illo adipisci quod quas? Sequi?</p>
         <button>INSCRÍBETE</button>
       </div>
 
       <div class="card">
         <img class= "picture" src="https://storage.googleapis.com/webdesignledger.pub.network/WDL/work-better-with-coders-1.jpg" alt="Denim Jeans" style="width:100%">
-        <h1>Taller 3</h1>
+        <h1>TALLER 3</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi dignissimos iste ex quasi quisquam et non porro hic ipsam tempora? Maiores vel impedit sint eum illo adipisci quod quas? Sequi?</p>
         <button>INSCRÍBETE</button>
       </div>
 
       <div class="card">
         <img class= "picture" src="https://storage.googleapis.com/webdesignledger.pub.network/WDL/work-better-with-coders-1.jpg" alt="Denim Jeans" style="width:100%">
-        <h1>Taller 4</h1>
+        <h1>TALLER 4</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi dignissimos iste ex quasi quisquam et non porro hic ipsam tempora? Maiores vel impedit sint eum illo adipisci quod quas? Sequi?</p>
         <button>INSCRÍBETE</button>
       </div>

0026279d822766d3aee553862d5b211cda775873
Mercedes <mvazquezsan@gmail.com>
2021-03-03 09:08:58 +0100

    workshopEdit.blade line deleted from dashboard.blade

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,7 +4,7 @@
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
-    {{-- <x-workshopEdit/> --}}
+    
 
     <a href="{{ route('workshopform') }}" class=" mt-2 block p-3 mx-auto bg-vermilion-500 text-alabaster-500 rounded w-40 text-center">Create Workshop </a>
 

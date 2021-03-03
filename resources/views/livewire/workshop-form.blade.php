8b02b7a883dea207c2ac78900d42118c38a1e26a
J-ordiEs <70259721+J-ordiEs@users.noreply.github.com>
2021-03-03 15:14:46 +0100

    modal crear taller ok

--- resources/views/livewire/workshop-form.blade.php
+++ resources/views/livewire/workshop-form.blade.php
@@ -8,47 +8,47 @@
         </div>
         <div class="absolute shadow-lg flex items-center justify-center ">
             <div class="bg-white rounded-lg shadow overflow-hidden  mx-auto p-4">
-                    <div>
-                        <label class="form-label my-2" for="title">
-                            Titulo
-                        </label>
-                        <input class="form-control" id="title" placeholder="Ingrese un Titulo">
-                    </div>
-                    <div>
-                        <label class="form-label my-2" for="image">
-                            Imagen
-                        </label>
-                        <input type="url" class="form-control" id="image" placeholder="Url Imagen">
-                    </div>
-                    <div>
-                        <label class="form-label my-2" for="description">
-                            Descripcion
-                        </label>
-                        <textarea class="form-control" id="description" placeholder="Ingrese una description"></textarea>
-                    </div>
-                    <div>
-                        <label class="form-label my-2" for="date">
-                            Fecha
-                        </label>
-                        <input type="date" class="form-control" id="date" placeholder="Ingrese un Titulo">
-                    </div>
-                    <div>
-                        <label class="form-label my-2" for="status">
-                            Estado
-                        </label>
-                        <select name="status" class="form-control" id="status">
-                            <option>
-                                Activo
-                            </option>
-                            <option>
-                                Desactivado
-                            </option>
-                        </select>
-                    </div>
-                    <div class="flex justify-between">
-                        <button class="bg-red-500 hover:bg-red-700 border-red-700 hover:text-white p-2 my-2 rounded">Cancelar</button>
-                        <button class="bg-green-500 hover:bg-green-700 border-green-700 hover:text-white p-2 my-2 rounded">Aceptar</button>
-                    </div>
+                <div class="w-auto md:w-96 lg:w-96 ">
+                    <label class="form-label my-2" for="title">
+                        Titulo
+                    </label>
+                    <input class="form-control" id="title" placeholder="Ingrese un Titulo">
+                </div>
+                <div>
+                    <label class="form-label my-2" for="image">
+                        Imagen
+                    </label>
+                    <input type="url" class="form-control" id="image" placeholder="Url Imagen">
+                </div>
+                <div>
+                    <label class="form-label my-2" for="description">
+                        Descripcion
+                    </label>
+                    <textarea class="form-control" id="description" placeholder="Ingrese una description"></textarea>
+                </div>
+                <div>
+                    <label class="form-label my-2" for="date">
+                        Fecha
+                    </label>
+                    <input type="date" class="form-control" id="date" placeholder="Ingrese un Titulo">
+                </div>
+                <div>
+                    <label class="form-label my-2" for="status">
+                        Estado
+                    </label>
+                    <select name="status" class="form-control" id="status">
+                        <option>
+                            Activo
+                        </option>
+                        <option>
+                            Desactivado
+                        </option>
+                    </select>
+                </div>
+                <div class="flex justify-between">
+                    <button class="bg-red-500 hover:bg-red-700 border-red-700 hover:text-white p-2 my-2 rounded">Cancelar</button>
+                    <button class="bg-green-500 hover:bg-green-700 border-green-700 hover:text-white p-2 my-2 rounded">Aceptar</button>
+                </div>
             </div>
         </div>
     </div>

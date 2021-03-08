58aac05ba28e4d06538b6ac3ac8235c8cb051f46
Olga <o.jpadilla73@gmail.com>
2021-03-08 12:46:30 +0100

    cambios secciones

--- resources/views/livewire/inscription.blade.php
+++ resources/views/livewire/inscription.blade.php
@@ -7,28 +7,28 @@
             <div class="bg-white rounded-lg shadow overflow-hidden  mx-auto p-4 bg-concrete-200">
                 <form>
                     <div class="px-3 mb-6 md:mb-0">
-                        <label class="uppercase tracking-wide text-vermilion-500 text-xs font-bold mb-2" for="company">
+                        <label class="uppercase tracking-wide text-vermilion-500 text-xs font-bold mb-2" for="nombre">
                             Nombre*
                         </label>
-                        <input class="w-full text-vermilion-500 bg-white border border-vermilion-500 rounded py-3 px-4 mb-3" id="company" type="text" placeholder="Nombre">
+                        <input class="w-full text-vermilion-500 bg-white border border-vermilion-500 rounded py-3 px-4 mb-3" type="text" placeholder="Nombre">
                     </div>
 
                     <div class="px-3 mb-6 md:mb-0">
-                        <label class="uppercase tracking-wide text-vermilion-500 text-xs font-bold mb-2" for="company">
-                            Apellidos*
+                        <label class="uppercase tracking-wide text-vermilion-500 text-xs font-bold mb-2" for="apellido">
+                            Apellido*
                         </label>
-                        <input class="w-full bg-white text-black border border-vermilion-500 rounded py-3 px-4 mb-3" id="company" type="text" placeholder="Apellidos">
+                        <input class="w-full bg-white text-black border border-vermilion-500 rounded py-3 px-4 mb-3" type="text" placeholder="Apellido">
                     </div>
 
                     <div class="px-3 mb-6 md:mb-0">
-                        <label class="uppercase tracking-wide text-vermilion-500 text-xs font-bold mb-2" for="company">
-                            Fecha de Nacimiento
+                        <label class="uppercase tracking-wide text-vermilion-500 text-xs font-bold mb-2" for="fecha de nacimiento">
+                            Fecha de Nacimiento*
                         </label>
-                        <input type="date" class="w-full text-black bg-white border-vermilion-500 rounded py-3 px-4 mb-3" id="company" placeholder="día/mes/año">
+                        <input type="date" class="w-full text-black bg-white border-vermilion-500 rounded py-3 px-4 mb-3" placeholder="día/mes/año">
                     </div>
 
                     <div class="px-3 mb-6 md:mb-0">
-                        <label class="uppercase tracking-wide text-vermilion-500 text-xs font-bold mb-2" for="company">
+                        <label class="uppercase tracking-wide text-vermilion-500 text-xs font-bold mb-2" for="genero">
                             Género
                         </label>
                         <div>
@@ -42,24 +42,24 @@
                     </div>
 
                     <div class="px-3 mb-6 md:mb-0">
-                        <label class="uppercase tracking-wide text-vermilion-500 text-xs font-bold mb-2" for="company">
+                        <label class="uppercase tracking-wide text-vermilion-500 text-xs font-bold mb-2" for="email">
                             eMail*
                         </label>
-                        <input class="w-full bg-white text-vermilion border border-vermilion-500 rounded py-3 px-4 mb-3" id="company" type="text" placeholder="@">
+                        <input class="w-full bg-white text-vermilion border border-vermilion-500 rounded py-3 px-4 mb-3" type="text" placeholder="@">
                     </div>
 
                     <div class="px-3 mb-6 md:mb-0">
-                        <label class="uppercase tracking-wide text-vermilion-500 text-xs font-bold mb-2" for="company">
+                        <label class="uppercase tracking-wide text-vermilion-500 text-xs font-bold mb-2" for="telefono">
                             Teléfono*
                         </label>
-                        <input class="w-full bg-white text-black border border-vermilion-500 rounded py-3 px-4 mb-3" id="company" type="text" placeholder="Teléfono">
+                        <input class="w-full bg-white text-black border border-vermilion-500 rounded py-3 px-4 mb-3" type="text" placeholder="Teléfono">
                     </div>
 
                     <div class="px-3 mb-6 md:mb-0">
-                        <label class="uppercase tracking-wide text-vermilion-500 text-xs font-bold mb-2" for="company">
+                        <label class="uppercase tracking-wide text-vermilion-500 text-xs font-bold mb-2">
                             Ciudad*
                         </label>
-                        <input class="w-full bg-white text-black border border-vermilion-500 rounded py-3 px-4 mb-3" id="company" type="text" placeholder="Ciudad">
+                        <input class="w-full bg-white text-black border border-vermilion-500 rounded py-3 px-4 mb-3" type="text" placeholder="Ciudad">
                     </div>
 
                     <div class="px-3 mb-6 md:mb-0">
@@ -68,7 +68,7 @@
                         </label>
                         <div>
                             <select class="w-full bg-white border-vermilion-500 text-vermilion-500 text-xs py-3 px-4 pr-8 mb-3 rounded" id="type text-vermilion-500">
-                                <option>Cuál?</option>
+                                <option>Selecciona...</option>
                                 <option>Entidad, fundación o programa social</option>
                                 <option>Redes Sociales</option>
                                 <option>Web de Factoría F5</option>

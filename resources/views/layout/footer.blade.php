8febf8ee0c04d6b6b064b9e586d32aff0bef5848
J-ordiEs <70259721+J-ordiEs@users.noreply.github.com>
2021-03-11 16:42:55 +0100

    layouts

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,27 @@
+        <footer class="flex bg-vermilion-500 h-30 static bottom-0 w-full pt-3">
+            <div class=" flex justify-between container mx-auto items-center h-full">
+                <div class="flex flex-col items-start">
+                    <div class="redes flex leading-tight mb-4">
+                        <img class="h-6 mx-2" src="./images/Face-b.png">
+                        <img class="h-6 mx-2" src="./images/Insta-b.png">
+                        <img class="h-6 mx-2" src="./images/Twitter-b.png">
+                        <img class="h-6 mx-2" src="./images/Linkedin-b.png">
+                        <img class="h-6 mx-2" src="./images/Youtube-b.png">
+                    </div>
+                    <div class="w-full flex flex-col leading-tight ml-2 mb-4 font-bold">
+                            <a class="block text-white  text-small" href="/privacy">Privacy policy</a>
+                            <a class="block text-white  text-small" href="/cookie">Cookie policy</a>
+                    </div>
+                </div>
+                <div class="mx-3 flex flex-col text-right">
+                    <h3 class="text-white text-xl ">CONTACTO</h3>
+                    <h3 class="text-white  text-small ">Depot Lab Coworking</h3>
+                    <h3 class="text-white  text-small ">(Carrer del Bruc, 149, 08037 Barcelona)</h3>
+                    <h3 class="text-white  text-small ">+34 658 56 30 97</h3>
+                </div>
+            </div>
+        </footer>
+
+    </body>
+</html>
+

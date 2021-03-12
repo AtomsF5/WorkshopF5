6b23549f236e7112341b5ae6adac08ee83fca18c
Jose <jmgarcia81@uoc.edu>
2021-03-12 10:38:46 +0100

    Texto botones cambiado

--- resources/views/livewire/slider.blade.php
+++ resources/views/livewire/slider.blade.php
@@ -8,28 +8,28 @@
         <img class= "picture" src="https://storage.googleapis.com/webdesignledger.pub.network/WDL/work-better-with-coders-1.jpg" alt="Denim Jeans" style="width:100%">
         <h1>TALLER 1</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi dignissimos iste ex quasi quisquam et non porro hic ipsam tempora? Maiores vel impedit sint eum illo adipisci quod quas? Sequi?</p>
-        <button>INSCRÍBETE</button>
+        <button>INSCRÍBEME</button>
       </div>
 
       <div class="card">
         <img class= "picture" src="https://storage.googleapis.com/webdesignledger.pub.network/WDL/work-better-with-coders-1.jpg" alt="Denim Jeans" style="width:100%">
         <h1>TALLER 2</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi dignissimos iste ex quasi quisquam et non porro hic ipsam tempora? Maiores vel impedit sint eum illo adipisci quod quas? Sequi?</p>
-        <button>INSCRÍBETE</button>
+        <button>INSCRÍBEME</button>
       </div>
 
       <div class="card">
         <img class= "picture" src="https://storage.googleapis.com/webdesignledger.pub.network/WDL/work-better-with-coders-1.jpg" alt="Denim Jeans" style="width:100%">
         <h1>TALLER 3</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi dignissimos iste ex quasi quisquam et non porro hic ipsam tempora? Maiores vel impedit sint eum illo adipisci quod quas? Sequi?</p>
-        <button>INSCRÍBETE</button>
+        <button>INSCRÍBEME</button>
       </div>
 
       <div class="card">
         <img class= "picture" src="https://storage.googleapis.com/webdesignledger.pub.network/WDL/work-better-with-coders-1.jpg" alt="Denim Jeans" style="width:100%">
         <h1>TALLER 4</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi dignissimos iste ex quasi quisquam et non porro hic ipsam tempora? Maiores vel impedit sint eum illo adipisci quod quas? Sequi?</p>
-        <button>INSCRÍBETE</button>
+        <button>INSCRÍBEME</button>
       </div>
 
     </div>

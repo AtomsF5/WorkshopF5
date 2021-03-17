2df2cc8487b5674b34af57369ecabd21c0c687c3
J-ordiEs <70259721+J-ordiEs@users.noreply.github.com>
2021-03-17 11:47:26 +0100

    boton guardar edit ok

--- resources/views/components/form-edit.blade.php
+++ resources/views/components/form-edit.blade.php
@@ -1,4 +1,4 @@
-<div class="w-5/6 pt-20 mx-auto">
+<div class="flex flex-col w-5/6 pt-20 mx-auto justify-items-center">
 
         <h1 class="p-10 text-3xl font-extrabold text-center font-poppins text-vermilion-500">Formulario para editar el taller: </br> {{$workshop->title}}</h1>
 
@@ -39,12 +39,14 @@
         <label for="platform_web" class="my-2 form-label" for="date">
             URL Página
         </label>
-        <input type="url" class="form-control" value="{{$workshop->platform_web}}"  name="platform_web" id="url" placeholder="URL Pagina">
+        <input type="url" class="mb-5 form-control" value="{{$workshop->platform_web}}"  name="platform_web" id="url" placeholder="URL Pagina">
 
 
 
         {{-- <button class="p-2 my-2 bg-red-500 border-red-700 rounded hover:bg-red-700 hover:text-white">Cancelar</button> --}}
-        <input type="submit" value="Guardar" class="p-2 my-2 bg-green-500 border-green-700 rounded hover:bg-green-700 hover:text-white">
+        <div class="flex justify-center">
+            <input type="submit" value="Guardar" class="items-center p-2 py-2 font-semibold text-white rounded font-poppins bg-vermilion-500 border-vermilion-700 hover:bg-vermilion-700 hover:text-white">
+        </div>
 
 </div>
 

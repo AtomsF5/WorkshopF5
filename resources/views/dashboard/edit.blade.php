94e6e36374ce6c5f5fd4364fc82301f474bf1444
J-ordiEs <70259721+J-ordiEs@users.noreply.github.com>
2021-03-17 11:41:06 +0100

    formulario de editar ok

--- resources/views/dashboard/edit.blade.php
+++ resources/views/dashboard/edit.blade.php
@@ -1,9 +1,12 @@
-@include('layout.header')
 
-    <form  class="mx-auto w-4/6" action="{{ url('/dashboard/'.$workshop->id) }}" method="post">
-        @csrf
-        @method('put')
-        @include('components.form-edit')
-    </form>
+<x-guest-layout>
 
-@include('layout.footer')
+        <form  class="w-4/6 mx-auto" action="{{ url('/dashboard/'.$workshop->id) }}" method="post">
+            @csrf
+            @method('put')
+            @include('components.form-edit')
+        </form>
+
+</x-guest-layout>
+
+

72ba6863a99979f7caaf6268690fb1feba9e0114
J-ordiEs <70259721+J-ordiEs@users.noreply.github.com>
2021-03-18 16:17:49 +0100

    landin page ok

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,6 @@
 @include('layout.header')
 
+    <div>
         <h2 class="p-10 text-3xl font-extrabold text-center font-poppins text-vermilion-500">Talleres Individuales</br></h1>
 
         <div class="grid gap-2 p-2 containerSlider lg:grid-cols-3 sm:grid-cols-1">
@@ -15,20 +16,18 @@
                 <p><strong>Fecha y Hora:</strong> {{$workshop->date}}</p>
                 <p><strong>Requerimientos Técnicos:</strong> {{$workshop->technical_requirement}}</p>
                 <div>
-
-                <x-inscriptionModal :workshop="$workshop"/>
-
+                    <x-inscriptionModal :workshop="$workshop"/>
                 </div>
             </div>
             @endforeach
+        </div>
+    </div>
 
+    <div>
         <h2 class="p-10 text-3xl font-extrabold text-center font-poppins text-vermilion-500">Talleres Grupales</br></h1>
 
         <div class="grid gap-2 p-2 containerSlider lg:grid-cols-3 sm:grid-cols-1">
-
-
             @foreach ($groups as $workshop)
-
             <div class="flex flex-col justify-between card">
                 <img class="picture" src="{{ asset('images/html-y-php-articulo.jpg') }}" alt= "imagen del taller">
                 <h1>{{$workshop->title}}</h1>
@@ -37,15 +36,14 @@
                 <p><strong>Fecha y Hora:</strong> {{$workshop->date}}</p>
                 <p><strong>Requerimientos Técnicos:</strong> {{$workshop->technical_requirement}}</p>
                 <div>
-
-                <x-inscriptionModal :workshop="$workshop"/>
-
+                    <x-inscriptionModal :workshop="$workshop"/>
                 </div>
             </div>
             @endforeach
-
-
         </div>
+    </div>
+
+
 
 
 @include('layout.footer')

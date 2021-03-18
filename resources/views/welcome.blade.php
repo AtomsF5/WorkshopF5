09684c4c59622fbfe7755c1e97033e71856f2e23
J-ordiEs <70259721+J-ordiEs@users.noreply.github.com>
2021-03-18 09:38:12 +0100

    welcome ok

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,32 +2,30 @@
 
     <livewire:landing-page :workshops="$workshops"></livewire:landing-page>
 
-<<<<<<< HEAD
-=======
     <h1 class="p-10 text-3xl font-extrabold text-center font-poppins text-vermilion-500">Talleres Individuales</br></h1>
-   
+
         <div class="grid gap-2 p-2 containerSlider lg:grid-cols-3 sm:grid-cols-1">
 
             {{-- <a href="#" id="left-arrow"><img src="images/Left.png" /></a>
             <a href="#" id="right-arrow"><img src="images/Right.png" /></a> --}}
-            
-            @foreach ($individuals as $workshop)            
+
+            @foreach ($individuals as $workshop)
 
             <div class="flex flex-col justify-between card">
                 <img class="picture" src="{{ asset('images/html-y-php-articulo.jpg') }}" alt= "imagen del taller">
                 <h1>{{$workshop->title}}</h1>
                 <p>{{$workshop->id}}</p>
                 <p><strong>Qué haremos:</strong> {{$workshop->description}}</p>
-                <p><strong>Tipo de taller:</strong> {{$workshop->category}}</p>                
+                <p><strong>Tipo de taller:</strong> {{$workshop->category}}</p>
                 <p><strong>Fecha y Hora:</strong> {{$workshop->date}}</p>
                 <p><strong>Requerimientos Técnicos:</strong> {{$workshop->technical_requirement}}</p>
                 <div>
-                
+
                     <x-button
                         class="px-8 py-3 bg-vermilion-500 hover:bg-vermilion-600 focus:outline-none focus:ring-2 focus:ring-vermilion-600 focus:border-transparent"
                         x-on:click="showModal= true"
                         >
-                        <a href="{{'#inscribeme', $workshopId=$workshop->id}}">Inscríbeme</a>                        
+                        <a href="{{'#inscribeme', $workshopId=$workshop->id}}">Inscríbeme</a>
                     </x-button>
                 </div>
             </div>
@@ -41,18 +39,18 @@
 
             {{-- <a href="#" id="left-arrow"><img src="images/Left.png" /></a>
             <a href="#" id="right-arrow"><img src="images/Right.png" /></a> --}}
-            
+
             @foreach ($groups as $workshop)
-            
+
             <div class="flex flex-col justify-between card">
                 <img class="picture" src="{{ asset('images/html-y-php-articulo.jpg') }}" alt= "imagen del taller">
                 <h1>{{$workshop->title}}</h1>
                 <p><strong>Qué haremos:</strong> {{$workshop->description}}</p>
-                <p><strong>Tipo de taller:</strong> {{$workshop->category}}</p>                
+                <p><strong>Tipo de taller:</strong> {{$workshop->category}}</p>
                 <p><strong>Fecha y Hora:</strong> {{$workshop->date}}</p>
                 <p><strong>Requerimientos Técnicos:</strong> {{$workshop->technical_requirement}}</p>
                 <div>
-                
+
                     <x-button
                         class="px-8 py-3 bg-vermilion-500 hover:bg-vermilion-600 focus:outline-none focus:ring-2 focus:ring-vermilion-600 focus:border-transparent"
                         x-on:click="showModal= true"
@@ -73,7 +71,7 @@
 <!-- Modal -->
 
         <x-modal trigger="showModal">
-        
+
         <form id="inscribeme" action="{{route('enroll', $workshopId)}}" method="POST" class="flex flex-col text-left">
                 @csrf
 
@@ -340,7 +338,7 @@
                     <x-button class="text-center bg-vermilion-500 hover:bg-vermilion-600 focus:outline-none focus:ring-2 focus:ring-vermilion-600 focus:border-transparent">
                         Inscríbeme
                     </x-button>
-                </div> 
+                </div>
             </form>-->
 
         </x-modal>
@@ -361,7 +359,6 @@
 
 
 
-    
-   
->>>>>>> f1f5916106aab22367682e63970e63ad240b5329
+
+
 @include('layout.footer')
